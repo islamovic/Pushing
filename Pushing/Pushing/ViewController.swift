73c238a35916600046336a07a0ca51218015835c
Islam Ibrahim <islam.abo.elsoud@gmail.com>
2021-03-24 14:23:43 +0200

    Project (test merge)
    * init project
    
    * print line

---
+++ Pushing/Pushing/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pushing
+//
+//  Created by Islam on 3/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        print("Line")
+    }
+
+
+}
+
